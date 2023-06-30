db004af3cc9978baa5160ea94cfdd9ba378a670b
Kirstin Penzes <kpenzes17@gmail.com>
2023-06-30 14:58:23 -0400

    Added new project seed for Pawsh, updated seed for FrontRow

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,25 @@ project.label = "Collaborative"
 project.name = "FrontRow"
 project.project_url = "https://www.front-row.site/"
 project.tagline = "The concert starts here."
-project.description = "A social platform where fans can log concerts they've seen by rating and reviewing their experience, follow other fans, and message them."
+project.description = "A social platform where fans can log concerts they've seen by rating and reviewing their experience, follow other fans, and message them. FrontRow is the final project for Le Wagon's web development bootcamp."
 project.github_url = "https://github.com/daouasof/frontrow"
 
 project.save!
 
 puts "frontrow project created"
+
+puts "Creating pawsh project..."
+
+project = Project.new
+project.start_date = Date.new(2023, 05, 29)
+project.end_date = Date.new(2023, 06, 02)
+project.label = "Collaborative"
+project.name = "Pawsh"
+project.project_url = "https://pawsh.herokuapp.com/"
+project.tagline = "Fabulous pets, for less."
+project.description = "An web app that allows users to browse pet outfits, rent them, and post their own outfits to rent. Pawsh is an Airbnb clone and one of two final projects for Le Wagon's web development bootcamp."
+project.github_url = "https://github.com/catagalan-mtl/pawsh"
+
+project.save!
+
+puts "pawsh project created"
