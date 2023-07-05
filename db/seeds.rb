9fb9030f4c1930f12d0cd9913bc21a153e1c94cf
Kirstin Penzes <kpenzes17@gmail.com>
2023-07-05 11:06:51 -0400

    Added photos for Pawsh project

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,32 @@ require "open-uri"
 puts "Clearing database..."
 Project.destroy_all
 
+puts "Creating pawsh project..."
+
+project = Project.new
+project.features = ["Browse all outfits", "Search by title, occasion, description using keywords", "View outfit on map", "Book an outfit", "List a new outfit for rent", "Accept or decline incoming requests"]
+project.start_date = Date.new(2023, 05, 29)
+project.end_date = Date.new(2023, 06, 02)
+project.label = "Collaborative"
+project.name = "Pawsh"
+project.project_url = "https://pawsh.herokuapp.com/"
+project.tagline = "Fabulous pets, for less."
+project.description = "An web app that allows users to browse pet outfits, rent them, and post their own outfits to rent. Pawsh is an Airbnb clone and one of two final projects for Le Wagon's web development bootcamp."
+project.github_url = "https://github.com/catagalan-mtl/pawsh"
+
+file = URI.open("https://i.ibb.co/s5nYNr1/pawsh-homepage.png")
+project.photos.attach(io: file, filename: "pawsh-homepage.png", content_type: "image/png")
+file = URI.open("https://i.ibb.co/1JC1HV7/Pawsh-homepage-2.png")
+project.photos.attach(io: file, filename: "pawsh-homepage-2.png", content_type: "image/png")
+file = URI.open("https://i.ibb.co/Bsd81dP/Pawsh-search-and-map.png")
+project.photos.attach(io: file, filename: "pawsh-search-and-map.png", content_type: "image/png")
+file = URI.open("https://i.ibb.co/wNzc1CF/Pawsh-book-outfit.png")
+project.photos.attach(io: file, filename: "pawsh-book-outfit.png", content_type: "image/png")
+
+project.save!
+
+puts "pawsh project created"
+
 puts "Creating frontrow project..."
 
 project = Project.new
@@ -35,22 +61,6 @@ project.save!
 
 puts "frontrow project created"
 
-puts "Creating pawsh project..."
-
-project = Project.new
-project.features = ["Browse all outfits", "Search by title, occasion, description using keywords", "View outfit on map", "Book an outfit", "List a new outfit for rent", "Accept or decline incoming requests"]
-project.start_date = Date.new(2023, 05, 29)
-project.end_date = Date.new(2023, 06, 02)
-project.label = "Collaborative"
-project.name = "Pawsh"
-project.project_url = "https://pawsh.herokuapp.com/"
-project.tagline = "Fabulous pets, for less."
-project.description = "An web app that allows users to browse pet outfits, rent them, and post their own outfits to rent. Pawsh is an Airbnb clone and one of two final projects for Le Wagon's web development bootcamp."
-project.github_url = "https://github.com/catagalan-mtl/pawsh"
-
-project.save!
-
-puts "pawsh project created"
 
 puts "Creating Portfolio project..."
 
